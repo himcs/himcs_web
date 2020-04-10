28b8f2328069fa6532f988e0f509a197ee1573b4
himcs <787632628@qq.com>
2020-04-10 09:08:44 +0800

    1

--- util/ssr.js
+++ util/ssr.js
@@ -1,7 +1,7 @@
 var process = require('child_process');
 
 let getSSR = (callback) => {
-    process.exec('v2ray ssinfo | grep "ss://" | sed s/"SS 链接 = "/""', function (error, stdout, stderr) {
+    process.exec('v2ray ssinfo | grep "ss://" | sed s/"SS 链接 = "/""/', function (error, stdout, stderr) {
         if (error !== null) {
             console.log('exec error: ' + error);
         }
